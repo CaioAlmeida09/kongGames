ffa90e6f8168ce30cfbb5caa97f6da57e53503f3
João Guilherme <joaoguilherme_pe@hotmail.com>
2025-03-17 15:54:50 -0300

    Feat: add changes label

--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -2,7 +2,7 @@ import { FaInstagram, FaWhatsapp } from 'react-icons/fa';
 
 export const LINKS = [
   {
-    label: 'Sistema da Kong Games',
+    label: 'Portal do Cliente',
     icon: (
       <div className="flex items-center gap-1">
         <div className="bg-black size-4 rounded-full flex justify-center items-center">
@@ -13,12 +13,12 @@ export const LINKS = [
     to: 'https://app.konggames.com.br/',
   },
   {
-    label: 'Grupo de Promoções',
+    label: 'Grupo VIP',
     icon: <FaWhatsapp size={20} color="#F96B04" />,
     to: 'https://chat.whatsapp.com/CxQ8ex7VeDFK73g99lAz6k',
   },
   {
-    label: 'Compras & Suporte',
+    label: 'Compre agora',
     icon: <FaWhatsapp size={20} color="#F96B04" />,
     to: 'https://wa.me/5581997893024?text=Ol%C3%A1%2C%20tenho%20interesse%20nos%20jogos%21',
   },
